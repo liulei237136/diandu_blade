9a3e551e84919fd9e4f13d6b3dfca33858a6ca61
liu lei <liulei237136@gmail.com>
2022-03-08 11:01:13 +0800

    找回密码成功消息提示

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -19,6 +19,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/home';
 
+    // protected  $namespace =  'App\\Http\\Controllers'; // 添加这一行
+
     /**
      * Define your route model bindings, pattern filters, etc.
      *
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\ConfirmPasswordController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\VerificationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,6 +29,31 @@ Route::get('/', function () {
 });
 
 // Auth::routes();
-Auth::routes(['verify' => true]);
+// Auth::routes(['verify' => true]);
+// 用户身份验证相关的路由
+// App\Http\Controllers\Auth
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class,'login']);
+Route::post('logout', [LoginController::class,'logout'])->name('logout');
+
+// 用户注册相关路由
+Route::get('register', [RegisterController::class,'showRegistrationForm'])->name('register');
+Route::post('register', [RegisterController::class,'register']);
+
+// 密码重置相关路由
+Route::get('password/reset', [ForgotPasswordController::class,'showLinkRequestForm'])->name('password.request');
+Route::post('password/email', [ForgotPasswordController::class,'sendResetLinkEmail'])->name('password.email');
+Route::get('password/reset/{token}', [ResetPasswordController::class,'showResetForm'])->name('password.reset');
+Route::post('password/reset', [ResetPasswordController::class,'reset'])->name('password.update');
+
+// 再次确认密码（重要操作前提示）
+Route::get('password/confirm', [ConfirmPasswordController::class,'showConfirmForm'])->name('password.confirm');
+Route::post('password/confirm', [ConfirmPasswordController::class,'confirm']);
+
+// Email 认证相关路由
+Route::get('email/verify', [VerificationController::class,'show'])->name('verification.notice');
+Route::get('email/verify/{id}/{hash}', [VerificationController::class,'verify'])->name('verification.verify');
+Route::post('email/resend', [VerificationController::class,'resend'])->name('verification.resend');
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

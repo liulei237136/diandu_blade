53d667ae03ec3f7be93e9afc7c249873eb82e363
liu lei <liulei237136@gmail.com>
2022-03-10 17:48:15 +0800

    添加数据库队列，redis在windows不好安装

--- app/Observers/RepositoryObserver.php
+++ app/Observers/RepositoryObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Repository;
 
 class RepositoryObserver
@@ -17,7 +18,10 @@ class RepositoryObserver
 
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
         if (!$repository->slug || $repository->isDirty('name')) {
-            $repository->slug = app(SlugTranslateHandler::class)->translate($repository->name);
+            // $repository->slug = app(SlugTranslateHandler::class)->translate($repository->name);
+
+            // 推送任务到队列
+            dispatch(new TranslateSlug($repository));
         }
     }
 }
